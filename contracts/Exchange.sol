7f3341ff05110c514fcc5532e5aff8b7558aa579
HowardLo <96519238+Howard-Lo-Taisys@users.noreply.github.com>
2022-09-05 18:17:04 +0800

    feat: add comments (#6)

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -6,7 +6,16 @@ import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+/// @title Exchange
+///
+/// @dev This contract is to exchange ETH or others erc20 token for VegasONE.
 contract Exchange is AccessControlEnumerable, ReentrancyGuard {
+
+    /**
+     * Global Variables, Struct
+     */
+
+    /// @dev The identifier of the role which maintains other settings.
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     struct ERC20TOKEN {
@@ -18,12 +27,24 @@ contract Exchange is AccessControlEnumerable, ReentrancyGuard {
         bool tokenStatus;
     }
 
+    /// @dev A mapping of all of the ERC20 token details.
     mapping(uint256 => ERC20TOKEN) public erc20Token;
-    
+
+    /// @dev The exchange rate of VegasONE for ETH.
     uint256 public ethRate;
+
+    /// @dev The minimum value of each exchange amount.
     uint256 public exchangeMinValue;
+
+    /// @dev The number of erc20 tokens in this contract.
     uint256 public tokenCount;
+
+    /// @dev A flag indicating whether VegasONE is exchangeable within the contract.
     bool internal enableExchange;
+
+    /**
+     * Events
+     */
 
     event addERC20TokenEvent(
         address indexed account,
@@ -48,6 +69,10 @@ contract Exchange is AccessControlEnumerable, ReentrancyGuard {
     event setExchangeMinValueEvent(uint256 amount);
     event EthDrawEvent(address indexed to, uint256 amount);
     event ERC20DrawEvent(address indexed tokenAddress, address indexed to, uint256 amount);
+
+    /**
+     * Constructor
+     */
 
     constructor(
         ERC20 VegasONEAddress,
@@ -121,26 +146,46 @@ contract Exchange is AccessControlEnumerable, ReentrancyGuard {
         tokenCount = 6;
     }
 
+    /**
+     * Modifier
+     */
+
+    /// @dev A modifier which asserts the caller has the admin role.
     modifier checkAdmin() {
         require(hasRole(ADMIN_ROLE, msg.sender), "Exchange: Only Admin can use.");
         _;
 	}
 
+    /// @dev A modifier which asserts the contract is enabled for exchanges.
     modifier checkIsEnableChange() {
         require(enableExchange, "Exchange: Cannot exchange now.");
         _;
     }
 
+    /// @dev A modifier which asserts that the tokenID exists.
     modifier checkTokenIDExist(uint256 tokenID) {
         require(address(erc20Token[tokenID].tokenAddress) != address(0), "Exchange: This token does not exist.");
         _;
     }
 
+    /// @dev A modifier which asserts that address is not a zero address.
     modifier checkAdress(address account) {
         require(account != address(0), "Exchange: Address can not be a zero address.");
         _;
     }
 
+    /**
+     * External/Public Functions
+     */
+
+    /// @dev Add a erc20 token to exchange for VegasONE.
+    ///
+    /// This function reverts if the caller does not have the admin role.
+    ///
+    /// @param tokenAddress         the address of the token.
+    /// @param tokenExchangeRate    the exchange rate of the tokens,
+    ///                             the entry value should be multiplied by the decimal of the selected token.
+    /// @param tokenStatus          ture if the token is exchangeable, false otherwise.
     function addERC20Token(
         ERC20 tokenAddress,
         uint256 tokenExchangeRate,
@@ -166,6 +211,11 @@ contract Exchange is AccessControlEnumerable, ReentrancyGuard {
         tokenCount++;
     }
 
+    /// @dev Convert the ETH to VEGASONE according to the exchanging rate.
+    ///
+    /// This function reverts if `isEnableExchange()` is false or if amount does not reach the minimum amount.
+    ///
+    /// @param walletAddress    the address to exchange tokens to.
     function ethToVegasONE(address walletAddress) external checkIsEnableChange nonReentrant payable {
         uint256 amount = msg.value * ethRate;
         require(amount >= exchangeMinValue, "Exchange: Minimum amount not reached.");
@@ -173,6 +223,15 @@ contract Exchange is AccessControlEnumerable, ReentrancyGuard {
         emit EthExchangeEvnet(walletAddress, msg.value, amount);
     }
 
+    /// @dev Convert the selected token to VEGASONE according to each exchange rate.
+    ///
+    /// This function reverts if `isEnableExchange()` is false, if eithor the selected tokenId does not exists,
+    ///  selected token status is false, or the amount does not reach the minimum amount.
+    ///
+    /// @param walletAddress    the address to exchange tokens to.
+    /// @param tokenID          the number of the selected token id.
+    /// @param amount           the amount of tokens to exchange,
+    ///                         the entry value should be multiplied by selected token decimals.
     function erc20ToVegasONE(
         address walletAddress,
         uint256 tokenID,
@@ -183,7 +242,9 @@ contract Exchange is AccessControlEnumerable, ReentrancyGuard {
         uint256 vegasONEAmount = amount * erc20Token[0].tokenDecimals 
             * token.tokenExchangeRate / token.tokenDecimals / token.tokenDecimals;
         require(vegasONEAmount >= exchangeMinValue, "Exchange: Minimum amount not reached.");
+        // transfer erc20 token to contract.
         require(token.tokenAddress.transferFrom(walletAddress, address(this), amount));
+        // transfer VegasONE to walletAddress.
         require(erc20Token[0].tokenAddress.transfer(walletAddress, vegasONEAmount));
         emit ERC20TokenExchangeEvnet(
             token.tokenAddress,
@@ -193,11 +254,24 @@ contract Exchange is AccessControlEnumerable, ReentrancyGuard {
         );
     }
 
+    /// @dev Withdraw the Eth within the contract to the assigned address.
+    ///
+    /// This function reverts if the caller does not have the admin role or if the assigned address is a zero address.
+    /// 
+    /// @param to       the address to withdraw tokens to.
+    /// @param amount   the amount of tokens to withdraw, the entry value should be multiplied by selected token decimals.
     function ethWithdraw(address to, uint256 amount) external checkAdmin checkAdress(to) nonReentrant {
         payable(to).transfer(amount);
         emit EthDrawEvent(to , amount);
     }
 
+    /// @dev Withdraw the selected tokens within the contract to the assigned address.
+    ///
+    /// This function reverts if the caller does not have the admin role or the assigned address is a zero address.
+    ///
+    /// @param tokenID  the number of the selected token id.
+    /// @param to       the address to withdraw tokens to.
+    /// @param amount   the amount of tokens to withdraw, the entry value should be multiplied by selected token decimals.
     function erc20Withdraw(
         uint256 tokenID,
         address to,
@@ -207,11 +281,22 @@ contract Exchange is AccessControlEnumerable, ReentrancyGuard {
         emit ERC20DrawEvent(address(erc20Token[tokenID].tokenAddress), to, amount);
     }
 
+    /// @dev Set the exchange rate for ETH.
+    ///
+    /// This function reverts if the caller does not have the admin role.
+    ///
+    /// @param rate the exchange rate of ETH.
     function setETHExchangeRate(uint256 rate) external checkAdmin {
         ethRate = rate;
         emit SetEthRateEvent(ethRate);
     }
 
+    /// @dev Set the exchange rate of VegasONE for the selected token.
+    ///
+    /// This function reverts if the caller does not have the admin role or the selected tokenId does not exists.
+    ///
+    /// @param tokenID  the number of the selected token id.
+    /// @param rate     the exchange rate of token, the entry value should be multiplied by selected token decimals.
     function setERC20TokenExchangeRate(uint256 tokenID, uint256 rate) 
         external 
         checkAdmin 
@@ -221,11 +306,19 @@ contract Exchange is AccessControlEnumerable, ReentrancyGuard {
         emit SetERC20ExchangeRateEvent(address(erc20Token[tokenID].tokenAddress), rate);
     }
 
+    /// @dev Set the exchangeable VegasONE status for ETH.
+    ///
+    /// This function reverts if the caller does not have the admin role.
     function setContractExchangeStatus() external checkAdmin {
         enableExchange = !enableExchange;
         emit SetContractExchangeStatusEvent(enableExchange);
     }
 
+    /// @dev Set the exchangeable VegasONE status for the selected token.
+    ///
+    /// This function reverts if the caller does not have the admin role or the selected token does not exist.
+    ///
+    /// @param tokenID the number of the selected token id.
     function setTokenExchangeStatus(uint256 tokenID) external checkAdmin checkTokenIDExist(tokenID) {
         erc20Token[tokenID].tokenStatus = !erc20Token[tokenID].tokenStatus;
         emit setERC20ExchangeStatusEvent(
@@ -234,11 +327,25 @@ contract Exchange is AccessControlEnumerable, ReentrancyGuard {
         );
     }
 
+    /// @dev Set the minimum exchange amount.
+    ///
+    /// This function reverts if the caller does not have the admin role.
+    ///
+    /// @param amount the amount of minimum exchange.
     function setExchangeMinValue(uint256 amount) external checkAdmin {
         exchangeMinValue = amount;
         emit setExchangeMinValueEvent(exchangeMinValue);
     }
 
+    /**
+     * View Functions
+     */
+
+    /// @dev Get the exchangeable VegasONE status of the contract.
+    ///
+    /// True if the state is exchangeable, false othewise.
+    ///
+    /// @return enableExchange the exchangeable VegasONE state.
     function isEnableExchange() external view returns (bool) {
         return enableExchange;
     }
